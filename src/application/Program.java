12a2a4e11400a51832b5671f610fdc94e278af86
Marlu.Patrocinio <marlupatrocinio01@gmail.com>
2023-01-25 09:58:32 -0300

    added some changes

--- src/application/Program.java
+++ src/application/Program.java
@@ -11,26 +11,26 @@ public class Program {
 
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
-		
+
 		Employee employee = new Employee();
-		
-		System.out.println("Name:");
+
+		System.out.println("Name: ");
 		employee.name = sc.nextLine();
-		
-		System.out.println("Gross Salary:");
+
+		System.out.println("Gross Salary: ");
 		employee.grossSalary = sc.nextDouble();
-		
-		System.out.println("Tax:");
+
+		System.out.println("Tax: ");
 		employee.tax = sc.nextDouble();
-		
+
 		System.out.println("Employee: " + employee.name + ", $" + employee.netSalary());
-		
+
 		System.out.println("wich percentagem to increase salary?");
 		double percentage = sc.nextDouble();
 		employee.increaseSalary(percentage);
-		
+
 		System.out.println("Updated data: " + employee.name + ", $" + employee.increaseSalary(percentage));
-	
+
 		sc.close();
 	}
 
